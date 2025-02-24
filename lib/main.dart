b35fb0e93c878358d8f30fc784199f9aff7315ac
monsalman <salmanfirdauas.m@gmail.com>
2025-02-24 12:09:18 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL'] ?? '',
+    anonKey: dotenv.env['SUPABASE_ANON_KEY'] ?? '',
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white ),
+        useMaterial3: true,
+      ),
+      // home: const SplashScreen(),
+    );
+  }
+}
+
+final Color Warna = Color.fromARGB(255, 158, 203, 238);
