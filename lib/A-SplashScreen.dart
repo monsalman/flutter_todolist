764ad1d97ed248c6894a88b4ca1770ebbe33cc10
monsalman <salmanfirdaus.m@gmail.com>
2025-03-10 13:38:51 +0700

    Login Verif

--- lib/A-SplashScreen.dart
+++ lib/A-SplashScreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todolist/Form/LoginPage.dart';
 import 'package:flutter_todolist/main.dart';
+import 'package:flutter_todolist/Navbar/NavBar.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -13,11 +15,27 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration(seconds: 3), () {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => LoginPage()),
-      );
-    });
+    _checkAuthState();
+  }
+
+  Future<void> _checkAuthState() async {
+    await Future.delayed(Duration(seconds: 3));
+
+    final Session? session = Supabase.instance.client.auth.currentSession;
+
+    if (mounted) {
+      if (session != null) {
+        // User is logged in, navigate to NavBar
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => NavBar()),
+        );
+      } else {
+        // User is not logged in, navigate to LoginPage
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => LoginPage()),
+        );
+      }
+    }
   }
 
   @override
